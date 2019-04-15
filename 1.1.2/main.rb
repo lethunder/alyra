855c3b03d64e2640b41c23d919ba25b5d7afa724
Didier BANLOCK <didier.banlock@gmail.com>
2019-04-15 10:23:06 +0200

    Terminé

---
+++ 1.1.2/main.rb
@@ -0,0 +1,25 @@
+# !/usr/bin/ruby
+
+def ask_user(min, max)
+  p "Selectionnez un chiffre entre #{min} et #{max}"
+	selected_number = gets
+	selected_number = selected_number.chomp.to_i
+end
+
+min = 1
+max = 100
+
+selected_number = ask_user(min, max)
+random_number = max
+
+loop do
+
+  if selected_number == random_number
+	  p "BRAVAOOOOOOOOOOOOOOOO!"
+	  break
+  end
+
+  random_number -= 1
+	p "FAUX. La valeur sélectionnée entree est incorrecte"
+  break if random_number == 1
+end 
