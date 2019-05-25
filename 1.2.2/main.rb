8c33983fa67da5d5d89357e947b7e77a49711f19
Didier BANLOCK <didier.banlock@gmail.com>
2019-05-25 21:17:34 +0200

    done

--- 1.2.2/main.rb
+++ 1.2.2/main.rb
@@ -4,7 +4,11 @@ taille_pourboire = {2000 => 13000, 6000 => 9000, 800 => 2000, 700 => 1500,	1200 
 nombre_max_octets = 6000
 
 def calculer_meilleur_pourboire(h={}, nombre_max_octets)
-	p h.keys.repeated_combination(h.keys.size).to_a.keep_if{|tab| tab.reduce(:+) <= nombre_max_octets}.sort_by { |tab| tab.map{|elt| h[elt]}.reduce(:+) }.last
+	combinaisons = []
+	1.upto(h.keys.size) do |index|
+	  h.keys.combination(index).to_a.each{|combinaison| combinaisons << combinaison}
+	end
+	p combinaisons.keep_if{|tab| tab.reduce(:+) <= nombre_max_octets}.sort_by { |tab| tab.map{|elt| h[elt]}.reduce(:+) }.last
 end
 
 calculer_meilleur_pourboire(taille_pourboire, nombre_max_octets)
